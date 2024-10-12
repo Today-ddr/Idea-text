0b805fd26bd9274eb6a168a56afc89b72a7086b4
Today <xiaoqianliya@gmail.com>
2024-10-12 23:05:30 +0800

    test1

---
+++ test.java
@@ -0,0 +1,10 @@
+/**
+ * @Package: PACKAGE_NAME
+ * @CreateTime: 2024-10-12 23:03
+ * @Version: 1.0
+ **/
+public class test {
+    public static void main(String[] args) {
+        System.out.println("11");
+    }
+}
